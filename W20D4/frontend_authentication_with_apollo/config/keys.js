280b770a01272645539c422c89eb6e8f0ae406a3
Bradley <capitalb@live.com>
2019-07-25 01:38:21 -0500

    Complete project Frontend Authentication With Apollo

---
+++ W20D4/frontend_authentication_with_apollo/config/keys.js
@@ -0,0 +1,4 @@
+module.exports = {
+  mongoURI: 'mongodb+srv://dev:sRP1mZtaljuEE8Xy@cluster0-igswh.mongodb.net/test?retryWrites=true&w=majority'
+  ,secretOrKey: "71985e8b-95ad-40b4-978e-65ecc1f40ee3"
+};
